186fe51fb5b4dd346bb48499203084dc851f3dcc
subin basnet <Basnetsubeen>
2022-08-11 15:57:06 +1000

    after refactor

--- src/middlewares/joi-validation/adminUserValidation.js
+++ src/middlewares/joi-validation/adminUserValidation.js
@@ -1,58 +1,40 @@
 import Joi from "joi";
+import {
+  ADDRESS,
+  FNAME,
+  LNAME,
+  EMAIL,
+  PASSWORD,
+  SHORTSTR,
+  validator,
+} from "./constant.js";
 
 export const newAdminUserValidation = (req, res, next) => {
-  try {
-    //define rules
-    const schmea = Joi.object({
-      fName: Joi.string().max(20).required(),
-      lName: Joi.string().max(20).required(),
-      email: Joi.string().email({ minDomainSegments: 2 }),
-      password: Joi.string().max(100).required(),
-      phone: Joi.string().max(100).required(),
-      address: Joi.string().max(100).allow("", null),
-      dob: Joi.date().allow("", null),
-    });
-    //give data to the rules
-    const { error } = schmea.validate(req.body);
-    if (error) {
-      error.status = 200;
-      return next(error);
-    }
-    next();
-  } catch (error) {
-    next(error);
-  }
+  //define rules
+  const schmea = Joi.object({
+    fName: FNAME.required(),
+    lName: LNAME.required(),
+    email: EMAIL.required(),
+    password: PASSWORD.required(),
+    phone: PHONE,
+    address: ADDRESS,
+    dob: DATE.allow("", null),
+  });
+  //give data to the rules
+  validator(schmea, req, res, next);
 };
 
 export const emailVerificationValidation = (req, res, next) => {
-  try {
-    const schmea = Joi.object({
-      email: Joi.string().email({ minDomainSegments: 2 }).required(),
-      emailValidationCode: Joi.string().max(100).required(),
-    });
-    const { error } = schmea.validate(req.body);
-    if (error) {
-      error.status = 200;
-      return next(error);
-    }
-    next();
-  } catch (error) {
-    next(error);
-  }
+  const schmea = Joi.object({
+    email: EMAIL.required(),
+    emailValidationCode: SHORTSTR.required(),
+  });
+  validator(schmea, req, res, next);
 };
 export const loginValidation = (req, res, next) => {
-  try {
-    const schmea = Joi.object({
-      email: Joi.string().email({ minDomainSegments: 2 }).required(),
-      password: Joi.string().max(100).required(),
-    });
-    const { error } = schmea.validate(req.body);
-    if (error) {
-      error.status = 200;
-      return next(error);
-    }
-    next();
-  } catch (error) {
-    next(error);
-  }
+  const schmea = Joi.object({
+    email: EMAIL.required(),
+    password: PASSWORD.required(),
+  });
+  validator(schmea, req, res, next);
 };
---
+++ src/middlewares/joi-validation/constant.js
@@ -0,0 +1,20 @@
+import Joi from "joi";
+export const FNAME = Joi.string().max(20);
+export const LNAME = Joi.string().max(20);
+export const EMAIL = Joi.string().email({ minDomainSegments: 2 });
+export const PASSWORD = Joi.string().max(100);
+export const PHONE = Joi.string().max(100);
+export const ADDRESS = Joi.string().max(100).allow("", null);
+export const DOB = Joi.date();
+
+export const SHORTSTR = Joi.string().max(100);
+export const LONGSTR = Joi.string().max(50000);
+
+export const validator = (schema, req, res, next) => {
+  const { error } = schema.validate(req.body);
+  if (error) {
+    error.status = 200;
+    return next(error);
+  }
+  next();
+};
